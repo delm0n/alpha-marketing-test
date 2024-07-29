28d782252c454f18761110c8936171d7b2f8bef9
delm0n <delm0n@mail.ru>
2024-07-30 02:16:18 +0300

    added header and features

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
 (function () {
-  console.log(1);
+  scrollFunction();
 })();
+
+function scrollFunction() {
+  $(window).scroll(function () {
+    if (window.innerWidth < 640) {
+      if ($(window).scrollTop() >= 40) {
+        $(".pagecrm_header").addClass("scroll");
+      } else {
+        $(".pagecrm_header").removeClass("scroll");
+      }
+    }
+  });
+}
