286112dd8820a2b4a49f0498c0cc60d82fffd4cb
delm0n <delm0n@mail.ru>
2024-07-29 19:06:16 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const gulp = require("gulp");
+const { src, series, parallel, dest } = require("gulp");
+
+const imagemin = require("gulp-imagemin"); // оптимизирует изображения
+const pngquant = require("imagemin-pngquant"); // качественно сжимает png
+const webp = require("gulp-webp"); // конвертирует изображения в webp
+
+const rigger = require("gulp-rigger");
+
+const gcmq = require("gulp-group-css-media-queries"); // сливает однинаковые media
+const cleancss = require("gulp-clean-css"); // минифицирует css
+const sass = require("gulp-sass")(require("sass"));
+
+// пути к файлам
+const paths = {
+  html: {
+    src: "src/*.html",
+    dest: "docs/",
+  },
+
+  css: {
+    src: "src/scss/main.scss",
+    dest: "style/",
+  },
+
+  img: {
+    src: "src/images/**.png",
+    dest: "docs/images/",
+  },
+};
+
+// оптимизация изображений
+function imgTask() {
+  return src(paths.img.src)
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({ quality: 80, progressive: true }),
+        pngquant({ quality: [0.8, 0.8] }),
+      ])
+    )
+    .pipe(gulp.dest(paths.img.dest));
+}
+function webpTask() {
+  return src(paths.img.src).pipe(webp()).pipe(gulp.dest(paths.img.dest));
+}
+
+//сборка html
+function htmlTask() {
+  return src(paths.html.src).pipe(rigger()).pipe(gulp.dest(paths.html.dest));
+}
+
+// сбор стилей
+function cssTask() {
+  return src(paths.css.src)
+    .pipe(sass())
+    .pipe(gcmq())
+
+    .pipe(
+      cleancss({
+        level: {
+          1: {
+            specialComments: 0,
+          },
+        },
+      })
+    )
+    .pipe(gulp.dest(paths.css.dest));
+}
+
+// запуск задач при изменении файлов
+gulp.task("watch", function () {
+  gulp.watch("src/**/*.html", parallel(htmlTask, cssTask));
+  gulp.watch(paths.img.src, parallel(imgTask, webpTask));
+  gulp.watch("src/scss/**/*.scss", parallel(htmlTask, cssTask));
+});
+
+exports.build = series(parallel(imgTask, webpTask, htmlTask, cssTask));
