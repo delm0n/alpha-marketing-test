4b4a1b8de0003429306194bfba86c275e60d769a
delm0n <delm0n@mail.ru>
2024-07-31 02:05:09 +0300

    added modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,31 @@
 (function () {
   scrollFunction();
+  //по всем элементам с классом fancybox открывать href
+  $(".fancybox").fancybox({
+    touch: false,
+  });
+
+  //по клику на форме открывать success-form
+  $("#call-form .main-button").on("click", function () {
+    $.fancybox.close();
+
+    setTimeout(() => {
+      $.fancybox.open({
+        src: "#success-form",
+        type: "inline",
+      });
+    }, 0);
+  });
+
+  $("#call-form label input").on("click", function () {
+    var btn = $("#call-form .main-button");
+
+    if (btn.attr("disabled")) {
+      btn.removeAttr("disabled");
+    } else {
+      btn.attr("disabled", "disabled");
+    }
+  });
 })();
 
 function scrollFunction() {
